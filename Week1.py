2bc0b35d244ce80e4f6d818f54978e6e75bbff68
jamiebellthomas <jamiebt@live.co.uk>
2023-01-24 14:49:51 +0000

    About to do exercise

--- Week1.py
+++ Week1.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python3
+"""
+This is a shebang/hash bang line. It tells the operating
+system's program loader to run this file with python3.
+This allows the programmer to control the script as opposed to the user
+"""
+# Once you've added a shebang line, you can make the file executable
+# by running the following command in the terminal:
+# chmod +x Week1.py
+# chmod means change mode
+# +x means add execute permissions
+# This will allow you to run the file by typing ./Week1.py in the terminal
+# If you run ls -l in the terminal, you'll see that the file has a * next to it
+# This means that the file is executable
 print("Hello World!")
+
+# mkdir (This is how we make a directory)
+# cd (This is how we change directories)
+# ls (This is how we list the contents of a directory)
+# pwd (This is how we print the working directory)
+# touch (This is how we create a file)
+# rm (This is how we remove a file)
+# rmdir (This is how we remove a directory)
+# cp [SOURCE (relative to current directory)][DESTINATION(relative to current directory)] (This is how we copy a file)
+# mv (This is how we move a file)
+# cat (This is how we print the contents of a file)
+# python/python3 (This is how we run a python file)
+# code (This is how we open a file in VS Code)
+# echo (Prints a string to the terminal)
+#notepad (This is how we open a file in Notepad, including .py files)
+
+print("This is a test")
