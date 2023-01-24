b4aa697fc89b359e29e0d245cfa3e71f0bf5722c
Jamie <jamiebt@live.co.uk>
2023-01-24 15:40:22 +0000

    End of week 1

--- cl-arguments.py
+++ cl-arguments.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 import sys
+print("This is the name of the script: ", sys.argv[0])
+number_of_args = len(sys.argv)
+print(number_of_args, "command line argument(s) inputted:")
+for arg in sys.argv:
+    print(arg)
 
-print("Hello World!")
